199cdd8e3406a62528157e2ab9de511c04a192cf
Amanda Slayton <alslayton86@gmail.com>
2018-02-27 19:46:42 -0600

    dinnertime- time to commit

--- array.js
+++ array.js
@@ -1,1 +1,25 @@
 console.log("Hello.");
+
+var a = [1,2,3];
+console.log(a.join(''));
+
+var b = "Join us";
+console.log(b.split());
+
+// CHAllENGE 1
+
+var test1 = 'cat';
+var test2 = 'racecar';
+var test3 = 'bear';
+var test4 = 'mom';
+
+
+//ANSWER
+var test = "cat";
+var opp = test.split("").reverse().join("");
+if (test === opp){
+  console.log("same?:", true);
+}else{
+  console.log("same?:", false);
+}
+
